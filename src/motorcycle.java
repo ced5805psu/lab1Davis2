f70ef01c049aecdf541298aa817a515ddfbd7555
Stromeagle2k <conman.ethan@gmail.com>
2026-01-20 11:44:41 -0500

    Added objects

--- src/motorcycle.java
+++ src/motorcycle.java
@@ -2,4 +2,12 @@ public class Motorcycle {
 
     private String color;
 
+    public Motorcycle(String color){
+        this.color = color;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
 }
